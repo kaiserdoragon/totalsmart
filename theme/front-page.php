040aae88d326940d3012e0d8b9e2475b21cedc33
QUORIX\quori <kaiserdoragon1121@gmail.com>
2026-01-19 16:26:05 +0900

    こんなお悩みありませんかの作成

--- theme/front-page.php
+++ theme/front-page.php
@@ -5,9 +5,9 @@
       <span>賢く安く簡単に最適なコスト削減</span>
       ワンストップで<br>全部解決！
     </h2>
-    <div class="mv--scroll_txt">
-      <div class="scroll">
-        <div class="left">
+    <div class="mv_scroll">
+      <div class="mv_scroll--inner">
+        <div class="mv_scroll--left">
           <p>
             Total Smartは経費削減を専門とする会社です。
             Total Smartは経費削減を専門とする会社です。
@@ -31,7 +31,7 @@
             Total Smartは経費削減を専門とする会社です。
           </p>
         </div>
-        <div class="right">
+        <div class="mv_scroll--right">
           <p>
             Total Smartは経費削減を専門とする会社です。
             Total Smartは経費削減を専門とする会社です。
@@ -57,12 +57,19 @@
         </div>
       </div>
     </div>
-    <div class="parallax-bg">
+    <div class="mv_parallax_bg">
       <video src="<?php echo get_template_directory_uri(); ?>/video/video.mp4" playsinline autoplay muted loop></video>
     </div>
   </div>
-  <section class="bg_white sec">
-    <h2>こんなお悩みありませんか？？</h2>
+  <section class="lead_worry bg_white sec">
+    <div class="container -md">
+      <h2>こんな<span class="lead_txt">お悩み</span>ありませんか？？</h2>
+      <ul>
+        <li>業務が忙しすぎる…<br>もっと<span class="lead_txt">効率化</span>をしたい！！</li>
+        <li>無駄な<br><span class="lead_txt">コスト・費用</span>を削りたい</li>
+        <li>情報の<span class="lead_txt">セキュリティ管理</span>を<br>徹底したい！</li>
+      </ul>
+    </div>
   </section>
 </main>
 <?php get_footer(); ?>
