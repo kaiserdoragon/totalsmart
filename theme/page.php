74f460449454cc7fc637ed7162d1ce642dfd8245
QUORIX\quori <kaiserdoragon1121@gmail.com>
2026-02-18 07:37:00 +0900

    固定ページの微調整

--- theme/page.php
+++ theme/page.php
@@ -12,9 +12,12 @@
 
 <?php $slug_name = $post->post_name; ?>
 <main class="<?php echo $slug_name; ?>_page">
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-      <?php the_content(); ?>
-      <?php endwhile; ?><?php endif; ?>
+  <div class="container sec -bottom">
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div>
 </main>
 
 <?php get_footer(); ?>
