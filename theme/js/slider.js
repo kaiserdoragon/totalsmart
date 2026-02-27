da7f3467149f39a8879fa3c7e61ae0c0afe13ea8
QUORIX\quori <kaiserdoragon1121@gmail.com>
2026-02-27 15:49:55 +0900

    採用情報のSPを作成

--- theme/js/slider.js
+++ theme/js/slider.js
@@ -20,7 +20,6 @@ const swiper = new Swiper(".works .swiper", {
     // 1025px以上
     1025: {
       slidesPerView: 5,
-      slidesPerView: 1.8,
     },
     // 1481px以上
     1481: {
@@ -35,10 +34,20 @@ const swiper = new Swiper(".works .swiper", {
 
 const recruitslider = new Swiper(".recruit_lead .swiper", {
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 2.5,
   speed: 6000,
   allowTouchMove: false,
   autoplay: {
     delay: 0,
+  },
+  breakpoints: {
+    // 481px以上
+    481: {
+      slidesPerView: 3,
+    },
+    // 1025px以上
+    1025: {
+      slidesPerView: 6,
+    },
   },
 });
