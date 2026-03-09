6c559e5cb0720566d268f94e2d39b21852d2b370
kaiserdoragon <kaiserdoragon1121@gmail.com>
2026-03-09 16:08:55 +0900

    GTM追加とエアコン関連にも追加

--- theme/airconshuuri_thanks.php
+++ theme/airconshuuri_thanks.php
@@ -25,6 +25,25 @@ if (function_exists('wp_robots_no_robots')) {
 <html <?php language_attributes(); ?>>
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-TKC4PWHS');
+  </script>
+  <!-- End Google Tag Manager -->
+
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, maximum-scale=1.0">
   <meta name="format-detection" content="telephone=no">
@@ -43,6 +62,11 @@ if (function_exists('wp_robots_no_robots')) {
 </head>
 
 <body <?php body_class(['shuurilp-thanks']); ?>>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKC4PWHS"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
   <?php wp_body_open(); ?>
 
   <header class="header">
