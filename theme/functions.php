4825ea02053fe1a3e1ddba9d6960213eabbc5f1b
kaiserdoragon <kaiserdoragon1121@gmail.com>
2026-06-17 15:00:49 +0900

    JSの圧縮

--- theme/functions.php
+++ theme/functions.php
@@ -722,11 +722,15 @@ if (!function_exists('theme_enqueue_js_only_optimized_assets')) {
     $register_local_script('scrollhint',   'js/scroll-hint.min.js',   array(), 'defer');
     $register_local_script('mainscripts',  'js/scripts.js',           array('jquery'), 'defer');
     // $register_local_script('prohibited',  'js/prohibited.js',         array('jquery'), 'defer');
-    $register_local_script('prohibited',  'js/simulation.js',         array('jquery'), 'defer');
+
     $register_local_script('slider',       'js/slider.js',            array('jquery', 'swiperjs'), 'defer');
 
     if (is_front_page()) {
       $register_local_script('loadinganimation', 'js/loadinganimation.js', array('jquery'), 'defer');
+    }
+
+    if (is_page('costaircon')) {
+      $register_local_script('prohibited',  'js/simulation.js',         array('jquery'), 'defer');
     }
 
     // post_type が 'information' の詳細ページの時だけ読み込む
